902c880c2f7dd6726415ffcdb656ae9419dba404
Nesrin Ghanem <ghanemnesrin@gmail.com>
2022-06-12 08:19:15 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(".nav-item").click(function(){
+       $("nav-icon").removeclass("active");
+       $(this).addclass("active");
+    });
+
+
+    $("#toggler").click(function(event) {
+        $('#wrap').toggleClass('toggled');
+        
+        var right = $('.sidebar').css("right");
+        if (right == '0px')
+        {
+            $('.sidebar').css({ 'right': '-17rem' });
+            $('.layer').fadeOut();
+        }
+        else{
+            $('.sidebar').css({ 'right': '0' });
+            $('.layer').fadeIn();
+        }
+    });
+
+    $('.layer').click(function () {
+        $('.sidebar').css({ 'right': '-17rem' });
+        $('.layer').fadeOut();
+    });
+
+    $(".search-icon").click(function () {
+        $(".search-input").slideToggle("slow"); 
+    });
+
+    
+    $(".search-icon").click(function(){
+        $(".search-input").slideToggle("slow");
+    })
+});
